9ddbb0242773d049ea4a4e1d06a04668ba8ab0fd
clara <clararestreporua22@gmail.com>
2022-05-02 14:03:59 -0500

    vainas de firebase

--- Js/registro.js
+++ Js/registro.js
@@ -10,9 +10,12 @@ botonregistro.addEventListener("click", function(event){
     event.preventDefault()
 
     //Capturar el valor escrito en la caja correo 
+    let email=document.getElementById("email").value
 
     //Capturar el escrito en la caja contraseña 
+    let password=document.getElementById("password").value
 
+    console.log(email,password)
 
     //Activar la rutina de firebase para el registro 
 
@@ -21,11 +24,13 @@ botonregistro.addEventListener("click", function(event){
     .then((userCredential) => {
     // Signed in
     const user = userCredential.user;
+    console.log("Éxito en el registro")
     // ...
     })
     .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
+    console.log("Fallo"+errorMessage)
     // ..
     });
 })
