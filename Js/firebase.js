2a6b3775823ac13a755d79846f270ca62db1fd3a
clara <clararestreporua22@gmail.com>
2022-05-02 02:22:39 -0500

    se agrega

---
+++ Js/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBsBzb_YQZW8biZwTGeq9CoXjDqwgLrsxg",
+  authDomain: "thestarvingvideojuego-9dd2b.firebaseapp.com",
+  projectId: "thestarvingvideojuego-9dd2b",
+  storageBucket: "thestarvingvideojuego-9dd2b.appspot.com",
+  messagingSenderId: "119808998471",
+  appId: "1:119808998471:web:f6acc9f49fbd1cef240562",
+  measurementId: "G-5760MFRK02"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
