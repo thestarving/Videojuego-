f681011394899a4c9ed9bc68b33f310b59688e4f
clara <clararestreporua22@gmail.com>
2022-05-10 14:26:10 -0500

    se hace un intento del login

--- Js/controladorindex.js
+++ Js/controladorindex.js
@@ -1,4 +1,12 @@
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js"
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js"
+
+//referencia al nombre de usuario o avatar 
+//referncia al boton de salir 
+
+let avatar=document.getElementById("username")
+let botonsalir=document.getElementById("botonsalir")
+let botonlogin=document.getElementById("btnlogin")
+
 
 const auth = getAuth();
 onAuthStateChanged(auth, (user) => {
@@ -7,6 +15,24 @@ onAuthStateChanged(auth, (user) => {
     // https://firebase.google.com/docs/reference/js/firebase.User
     const uid = user.uid;
     console.log("Usuario autenticado "+user.email)
+    avatar.textContent=user.email
+    avatar.classList.remove("invisible")
+    botonlogin.classList.add("invisible")
+
+    //Rutina de salida
+    botonsalir.addEventListener("click", function(evento){
+
+      signOut(auth).then(() => {
+        // Sign-out successful.
+        avatar.classList.add("invisible")
+        botonlogin.classList.remove("invisible")
+        window.location=href="index.html"
+
+      }).catch((error) => {
+        // An error happened.
+      });
+
+    })
     // ...
   } else {
     // User is signed out
